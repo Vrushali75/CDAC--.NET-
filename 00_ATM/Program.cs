4aba2cc8669dd6760cdbaaa5c628f995bd6d5841
Vrushali Varankar <varankarvrushali07@gmail.com>
2024-05-30 02:06:06 +0530

    Add files via upload

---
+++ 00_ATM/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace _00_ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int main_bal = 1000000;
+            int pin = 9632;
+            int[] arr =new int[3];//arr[3]
+            Console.WriteLine("Enter your pin: ");
+            int pin1 = Convert.ToInt32(Console.ReadLine());
+            if(pin==pin1)
+            {
+               do
+               {
+                    Console.WriteLine("1)Credit Amount\n2)Withdraw Amount\n3)Check Balance\n");
+                
+                    int ch = Convert.ToInt32(Console.ReadLine());
+                
+                    switch (ch)
+                    {
+                        case 1:
+                            Console.WriteLine("----------Credit Amount----------");
+                            Console.WriteLine("Enter amount to be credited: ");
+                            int cred_amt = Convert.ToInt32(Console.ReadLine());
+                            main_bal = main_bal + cred_amt;
+                            Console.WriteLine("Total balance after credited amount is: " + main_bal);
+                            break;
+
+                        case 2:
+                            Console.WriteLine("-----------Withdraw Amount----------");
+                            Console.WriteLine("Enter the withdrawl amount: ");
+                            int withdraw_amt = Convert.ToInt32(Console.ReadLine());
+                            main_bal = main_bal - withdraw_amt;
+                            Console.WriteLine("Total Balance after withdrwal amount is: " + main_bal);
+                            break;
+
+                        case 3:
+                            Console.WriteLine("----------Remaining Balance----------");
+                            Console.WriteLine("Remaining Balance is :" + main_bal);
+                            break;
+
+                        default:
+                            Console.WriteLine("Enter valid choice !");
+                            break;
+
+                    }
+                    Console.WriteLine("If you want to continue, press Y");
+                    char ch1 = Convert.ToChar(Console.ReadLine());
+                    if (ch1 == 'Y' || ch1=='y') 
+                        continue;
+                    else
+                    {
+                        Console.WriteLine("Thank You, Visit again !");
+                        break;
+                    }
+                        
+                } while (true);
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid pin!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01_InterfaceDemo/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_InterfaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            char c;
+            do
+            {
+                Console.WriteLine("1)SQL Server\n2)Oracle Server\n3)MySQL Server");
+                Console.WriteLine("Enter your choice: ");
+                ch = Convert.ToInt32(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+
+                        SQL sqlObj = new SQL();
+                        Console.WriteLine("----SQL Server----");
+                        Console.WriteLine("1)Insert\n2)Update\n3)Delete");
+                        Console.WriteLine("Enter your database operation:");
+                        ch = Convert.ToInt32(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case 1:
+                                sqlObj.insert();
+                                break;
+
+                            case 2:
+                                sqlObj.update();
+                                break;
+
+                            case 3:
+                                sqlObj.delete();
+                                break;
+                        }
+                        break;
+
+                    case 2:
+
+                        Oracle oracleObj = new Oracle();
+                        Console.WriteLine("----Oracle Server----");
+                        Console.WriteLine("1)Insert\n2)Update\n3)Delete");
+                        Console.WriteLine("Enter your database operation:");
+                        ch = Convert.ToInt32(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case 1:
+                                oracleObj.insert();
+                                break;
+
+                            case 2:
+                                oracleObj.update();
+                                break;
+
+                            case 3:
+                                oracleObj.delete();
+                                break;
+                        }
+                        break;
+
+                    case 3:
+
+                        MySQL mysqlObj = new MySQL();
+                        Console.WriteLine("----MySQL Server----");
+                        Console.WriteLine("1)Insert\n2)Update\n3)Delete");
+                        Console.WriteLine("Enter your database operation:");
+                        ch = Convert.ToInt32(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case 1:
+                                mysqlObj.insert();
+                                break;
+
+                            case 2:
+                                mysqlObj.update();
+                                break;
+
+                            case 3:
+                                mysqlObj.delete();
+                                break;
+                        }
+                        break;
+
+                }
+                Console.WriteLine("Do you want to continue? press Y/N");
+                c =Convert.ToChar( Console.ReadLine());
+                if (c == 'y' || c == 'Y')
+                    continue;
+                else
+                    break;
+
+            } while (true);
+            
+            Console.ReadLine();
+        }
+        
+
+    }
+    public class SQL
+    {
+        public void insert()
+        {
+            Console.WriteLine("Element inserted at SQL Server");
+        }
+        public void update()
+        {
+            Console.WriteLine("Element updated at SQL Server");
+        }
+        public void delete()
+        {
+            Console.WriteLine("Element deleted at SQL Server");
+        }
+    }
+    public class Oracle
+    {
+        public void insert()
+        {
+            Console.WriteLine("Element inserted at Oracle Server");
+        }
+        public void update()
+        {
+            Console.WriteLine("Element updated at Oracle Server");
+        }
+        public void delete()
+        {
+            Console.WriteLine("Element deleted at Oracle Server");
+        }
+    }
+    public class MySQL
+    {
+        public void insert()
+        {
+            Console.WriteLine("Element inserted at MySQL Server");
+        }
+        public void update()
+        {
+            Console.WriteLine("Element updated at MySQL Server");
+        }
+        public void delete()
+        {
+            Console.WriteLine("Element deleted at MySQL Server");
+        }
+    }
+}
